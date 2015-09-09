d81b7dec32e2c25b0686cb814a424792bf05b74f
Aziz Alto <iamaziz.alto@gmail.com>
2015-09-09 01:58:35 -0400

    conform to PyPI

--- setup.py
+++ setup.py
@@ -8,17 +8,16 @@ except ImportError:
 
 setup(
     name='TermFeed',
-    summary='Minimal feed reader for terminal.',
-    description='Browse, read, and open your favorite rss feed in the terminal (without curses).',
+    description=('Browse, read, and open your favorite rss feed in the terminal (without curses).'),
     author='Aziz Alto',
     url='https://github.com/iamaziz/TermFeed',
     download_url='https://github.com/iamaziz/TermFeed/archive/master.zip',
+    license = "MIT",
     author_email='iamaziz.alto@gmail.com',
     version='0.0.5',
     install_requires=['feedparser'],
     packages=['termfeed', 'termfeed.support'],
     scripts=[],
-    keyword='terminal rss feed reader command line',
     entry_points={
         'console_scripts': [
             'feed = termfeed.feed:main'
